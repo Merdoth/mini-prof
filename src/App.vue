766990f397ea4ac72f80338ca95d6688c73ef07f
Merdoth <ucheya97@gmail.com>
2021-02-24 17:46:09 +0100

    Includes ui implementation and initial styling

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
-  <SearchUser />
+ <div class="home">
+    <SearchUser />
   <UserList />
+ </div>
 </template>
 
 <script>
@@ -18,12 +20,42 @@ export default {
 
 <style>
 @import '../node_modules/@fortawesome/fontawesome-free/css/all.css';
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+*,
+*:before,
+*:after {
+  margin: 0;
+  padding: 0;
+}
+body {
+  background-color: #eeeeee;
+}
+.home {
+  font-family: sans-serif;
+  width: 564px;
+  height: 643px;
+  margin: 40px auto;
+  left: 0;
+  right: 0;
+  padding: 20px;
+  background-color: white;
+  position: fixed;
+  overflow-y: scroll;
+}
+.home::-webkit-scrollbar {
+  width: 4px;
+}
+/* Track */
+.home::-webkit-scrollbar-track {
+  background: #f1f1f1;
+}
+/* Handle */
+.home::-webkit-scrollbar-thumb {
+  background: #4d4d4d;
+  border-radius: 5px;
+  height: 40px;
+}
+/* Handle on hover */
+.home::-webkit-scrollbar-thumb:hover {
+  background: #555;
 }
 </style>
